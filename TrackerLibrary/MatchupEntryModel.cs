592094e336291cb663dee2391a61b3ea24777119
cidospark <cidospark27@gmail.com>
2020-03-30 16:34:22 +0100

    Added initial class models

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        // Represemts one team in the matchup
+        public TeamModel TeamCompeting { get; set; }
+
+        // The score for this particular team
+        public double Score { get; set; }
+
+        // The matchup that this team came from as the winner
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
